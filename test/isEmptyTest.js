64c018e2bff90b6d1beb82e021ea2975aeaac1b3
Roosa Nurminen <roosa.nurminen@tuni.fi>
2025-11-23 16:25:53 +0200

    Add unit tests for isEmpty and toNumber

---
+++ test/isEmptyTest.js
@@ -0,0 +1,106 @@
+import { assert } from "chai";
+import isEmpty from "../src/isEmpty.js"
+
+describe("Positive test cases for isEmpty.js", () => {
+
+    // Arrays
+    it("Should return true if array is empty", () => {
+        assert.equal(isEmpty([]), true);
+    });
+    it("Should return false if array isn't empty", () => {
+        assert.equal(isEmpty([1, 2, 3]), false);
+    });
+
+    // null, undefined, NaN
+    it("Should return true if value is null", () => {
+        assert.equal(isEmpty(null), true);
+    });
+    it("Should return true if value is undefined", () => {
+        assert.equal(isEmpty(undefined), true);
+    });
+    it("Should return true if value is NaN", () => {
+        assert.equal(isEmpty(NaN), true);
+    });
+
+    // Objects
+    it("Should return true if object is empty", () => {
+        assert.equal(isEmpty({}), true);
+    });
+    it("Should return false if object is not empty", () => {
+        assert.equal(isEmpty({'a': 1}), false);
+    });
+   
+    // Strings
+    it("Should return true if string is empty", () => {
+        assert.equal(isEmpty(""), true);
+    });
+    it("Should return false if string is not empty", () => {
+        assert.equal(isEmpty("abc"), false);
+    });
+
+    // Booleans
+    it("Should return true if value is true", () => {
+        assert.equal(isEmpty(true), true);
+    });
+    it("Should return true if value is false", () => {
+        assert.equal(isEmpty(false), true);
+    });
+
+    // Numbers
+    it("Should return true for positive integer", () => {
+        assert.equal(isEmpty(1), true);
+    });
+    it("Should return true for negative integer", () => {
+        assert.equal(isEmpty(-1), true);
+    });
+    it("Should return true for positive decimal", () => {
+        assert.equal(isEmpty(1.1), true);
+    });
+    it("Should return true for negative decimal", () => {
+        assert.equal(isEmpty(-1.1), true);
+    });
+
+    // Maps
+    it("Should return true if maps is empty", () => {
+        const map = new Map()
+        assert.equal(isEmpty(map), true);
+    });
+    it("Should return false if map is not empty", () => {
+        const map = new Map([["A", 1], ["B", 2]]);
+        assert.equal(isEmpty(map), false);
+    });
+
+    // Sets
+    it("Should return true if set is empty", () => {
+        const set = new Set();
+        assert.equal(isEmpty(set), true);
+    });
+    it("Should return false if set is not empty", () => {
+        const set = new Set(["A", "B"]);
+        assert.equal(isEmpty(set), false);
+    });
+
+    // Buffers
+    it("Should return true if Buffer is empty", () => {
+        const buffer = Buffer.alloc(0);
+        assert.equal(isEmpty(buffer), true);
+    });
+    it("Should return false if Buffer is not empty", () => {
+        const buffer = Buffer.alloc(10)
+        assert.equal(isEmpty(buffer), false);
+    });
+
+    // Arguments
+    it("Should return true if arguments object is empty", () => {
+        function test() {
+            return isEmpty(arguments)
+        }
+        assert.equal(test(), true);
+    });
+    it("Should return false if arguments objcet is not empty", () => {
+        function test() {
+            return isEmpty(arguments)
+        }
+        assert.equal(test(1,2), false);
+    });
+});
---
+++ test/toNumberTest.js
@@ -0,0 +1,80 @@
+import { assert } from "chai";
+import toNumber from "../src/toNumber.js"
+
+describe("Positive testing for toNumber.js", () => {
+
+    // Numbers
+    it("Should return value itself when it's a positive integer", () => {
+        assert.equal(toNumber(3), 3);
+    });
+    it("Should return value itself when it's a negative integer", () => {
+        assert.equal(toNumber(-3), -3);
+    });
+    it("Should return value itself when it's a positve decimal", () => {
+        assert.equal(toNumber(3.2), 3.2);
+    });
+    it("Should return value itself when it's a negative decimal", () => {
+        assert.equal(toNumber(-3.2), -3.2);
+    });
+
+    // Strings
+    it("Should return string as int when string value is integer", () => {
+        assert.equal(toNumber("3"), 3);
+    });
+    it("Should return string as int when string value is negative integer", () => {
+        assert.equal(toNumber("-3"), -3);
+    });
+    it("Should return string as float when string value is positve decimal", () => {
+        assert.equal(toNumber("3.2"), 3.2);
+    });
+    it("Should return string as float when string value is negative decimal", () => {
+        assert.equal(toNumber("-3.2"), -3.2);
+    });
+    it("Should return number when string contains whitespaces also", () => {
+        assert.equal(toNumber("  10 "), 10);
+    });
+    it("Should return NaN if string is not number", () => {
+        assert.isNaN(toNumber("Hi"));
+    });
+
+    // Objects, maps, sets
+    it("Should return NaN if value is object", () => {
+        assert.isNaN(toNumber({"a": 1}));
+    });
+    it("Should return NaN if value is map", () => {
+        const map = new Map()
+        assert.isNaN(toNumber(map));
+    });
+    it("Should return NaN if value is set", () => {
+        const set = new Set([1, 2]);
+        assert.isNaN(toNumber(set));
+    });
+
+    // Infinity
+    it("Should return Infinity if value is Infinity", () => {
+        assert.equal(toNumber(Infinity), Infinity);
+    });
+
+    // Symbol
+    it("Should return NaN if value is Symbol", () => {
+        assert.isNaN(toNumber(Symbol('foo')));
+    });
+
+    // Binary
+    it("Should correctly parse valid binary string", () => {
+        assert.equal(toNumber("0b101"), 5);
+    });
+    
+    // Hexadecimals
+    it("Should correctly parse valid hex value", () => {
+        assert.equal(toNumber("0xFF"), 255);
+    });
+    it("Should return NaN for invalid signed hex", () => {
+        assert.isNaN(toNumber("-0x10"));
+    });
+    
+    // Octals
+    it("Should correctly parse valid octal", () => {
+        assert.equal(toNumber(0o10), 8);
+    });
+});

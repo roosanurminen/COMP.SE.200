aba9c6148fe79f95c9cce1c860c0f37c613852e2
Roosa Nurminen <roosa.nurminen@tuni.fi>
2025-11-23 19:58:58 +0200

    Add unit test for toString

--- test/addTest.js
+++ test/addTest.js
@@ -4,58 +4,44 @@ import add from "../src/add.js"
 describe("Positive testing for add.js", () => {
 
     it("It should sum two positive integers correctly", () =>{
-
         const augend = 6;
         const addend = 4;
-
         assert.equal(add(augend, addend), 10);
     })
 
     it("It should sum two negative integers correctly", () =>{
-
         const augend = -2;
         const addend = -8;
-
         assert.equal(add(augend, addend), -10);
     })
 
     it("It should sum positive and negative integers correctly", () =>{
-
         const augend = -4;
         const addend = 6;
-
         assert.equal(add(augend, addend), 2);
     })
 
     it("It should sum positive integer and zero addend correctly", () =>{
-
         const augend = 3;
         const addend = 0;
-
         assert.equal(add(augend, addend), 3);
     })
 
     it("It should sum zero augend and negative integer correctly", () =>{
-
         const augend = 0;
         const addend = -2;
-
         assert.equal(add(augend, addend), -2);
     })
 
     it("It should sum two positive decimals correctly", () =>{
-
         const augend = 2.1;
         const addend = 3.3;
-
         assert.equal(add(augend, addend), 5.4);
     })
 
     it("It should sum positve decimal and integer correctly", () =>{
-
         const augend = 2.1;
         const addend = 4;
-
         assert.equal(add(augend, addend), 6.1);
     })
 
@@ -65,34 +51,26 @@ describe("Positive testing for add.js", () => {
 describe("Negative testing for add.js", () => {
 
     it("It should throw error if augend is undefined", () =>{
-
         const augend = undefined;
         const addend = 2;
-
         expect(() => add(augend, addend)).to.throw(error)
     })
 
     it("It should throw error if addend is undefined", () =>{
-
         const augend = 3;
         const addend = undefined;
-
         expect(() => add(augend, addend)).to.throw(error)
     })
 
     it("It should throw error if augend is string", () =>{
-
         const augend = "6";
         const addend = 4;
-
         expect(() => add(augend, addend)).to.throw(error);
     })
 
     it("It should throw error if addend is string", () =>{
-
         const augend = 6;
         const addend = "4";
-
         expect(() => add(augend, addend)).to.throw(error);
     })
 })
---
+++ test/toStringTest.js
@@ -0,0 +1,75 @@
+import { assert } from "chai";
+import toString from "../src/toString.js"
+
+describe("Positive testing for toString.js", () => {
+
+    // Null, undefined
+    it("Should return empty string for null", () => {
+        assert.equal(toString(null), "");
+    });
+    it("Should return empty string for undefined", () => {
+        assert.equal(toString(undefined), "");
+    });
+
+    // Numbers
+    it("Should return positive integer as string", () => {
+        assert.equal(toString(4), "4");
+    });
+    it("Should preserve the sign for '-0'", () => {
+        assert.equal(toString(-0), "-0");
+    });
+    it("Should return positive decimal as string", () => {
+        assert.equal(toString(0.1), "0.1");
+    });
+    it("Should preserve the sign for '-4.1'", () => {
+        assert.equal(toString(-4.1), "-4.1");
+    });
+
+    // Arrays
+    it("Should return array as string", () => {
+        assert.equal(toString([1,2,3]), "1,2,3");
+    });
+    it("Should return empty array as empty string", () => {
+        assert.equal(toString([]), "");
+    });
+
+    // Strings
+    it("Should return string as string", () => {
+        assert.equal(toString("5"), "5");
+    });
+
+    // Symbols
+    it("Should return symbol as string", () => {
+        const symb = Symbol("x")
+        assert.equal(toString(symb), "Symbol(x)");
+    });
+
+    // Booleans
+    it("Should return boolean as string", () => {
+        assert.equal(toString(true), "true");
+    });
+    it("Should return boolean as string", () => {
+        assert.equal(toString(false), "false");
+    });
+
+    // Maps, Objects, Sets, Nan, infinity
+    it("Should work with sets ", () => {
+        assert.equal(toString(new Set([1,2])), "[object Set]");
+    });
+    it("Should work with maps ", () => {
+        assert.equal(toString(new Map([[1, 2]])), "[object Map]");
+    });
+    it("Should work with objects ", () => {
+        assert.equal(toString({'a':1}), "[object Object]");
+    });
+    it("Should return NaN as string ", () => {
+        assert.equal(toString(NaN), "NaN");
+    });
+    it("Should return Infinity as string ", () => {
+        assert.equal(toString(Infinity), "Infinity");
+    });
+    it("Should return -Infinity as string ", () => {
+        assert.equal(toString(-Infinity), "-Infinity");
+    });
+
+});
